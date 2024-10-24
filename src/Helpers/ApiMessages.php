647579132af88fb28472c40a9690d91422df33d2
divineion <najima.guermoudi@gmail.com>
2024-10-24 18:43:54 +0200

    src/Helpers/ApiMessages.php : create file to store message constants

---
+++ src/Helpers/ApiMessages.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers;
+
+readonly class ApiMessages
+{
+
+    //comptes utilisateurs
+    const ACCOUNT_CREATION_FAILURE = "Une erreur est survenue pendant la création du compte. Veuillez réessayer ultérieurement.";
+
+
+}
