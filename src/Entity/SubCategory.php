fe2f35fef12a73929927b91dd7471732ac1ceb57
divineion <najima.guermoudi@gmail.com>
2024-10-24 18:31:58 +0200

    src/Entity/SubCategory.php : - add file with : - id and name properties,  - $recipes property to define a OneToMany relationship with the Recipe entity (one recipe is associated with a single subcategory while one subcategory can contain multiple recipes in a collection)  - $categories property to define a ManyToOne relationship with the Category entity (one subcategory is associated with a single category while one category can contain many subcategories)

---
+++ src/Entity/SubCategory.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubCategoryRepository::class)]
+#[ORM\Table(name: 'subcategory')]
+class SubCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Recipe>
+     */
+    #[ORM\OneToMany(targetEntity: Recipe::class, mappedBy: 'subCategory')]
+    private Collection $recipes;
+
+    public function __construct()
+    {
+        $this->recipes = new ArrayCollection();
+    }
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'subCategories')]
+    private ?Category $category;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getRecipes(): Collection
+    {
+        return $this->recipes;
+    }
+
+    public function addRecipe(Recipe $recipe): self
+    {
+        if (!$this->recipes->contains($recipe)) {
+            $this->recipes->add($recipe);
+            $recipe->setSubCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipe(Recipe $recipe): self
+    {
+        if ($this->recipes->removeElement($recipe)) {
+            $recipe->setSubCategory(null);
+        }
+
+        return $this;
+    }
+}
