f885e0edd7bc193a843fd6270c95c624c927c27a
divineion <najima.guermoudi@gmail.com>
2024-10-24 18:53:06 +0200

    src/Repository/RecipeRepository.php : add file; add findOneById(), findRecipesByUserId() and findAll() methods to manage recipes data retrieval

---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipe>
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    //    /**
+    //     * @return Recipe[] Returns an array of Recipe objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('r.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Recipe
+    //    {
+    //        return $this->createQueryBuilder('r')
+    //            ->andWhere('r.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+
+    public function findRecipeById($value): ?Recipe
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.id = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    public function findRecipesByUserId($value): array
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.user = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findAll(): array
+    {
+        $qb = $this->createQueryBuilder('r')
+                ->select('r.id', 'r.title', 'r.instructions');
+        return $qb->getQuery()->getResult();
+    }
+}
