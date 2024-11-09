08ba1806ab0efcd7b04314f5fdd7067e0fc97b16
divineion <najima.guermoudi@gmail.com>
2024-11-09 17:13:56 +0100

    src/Helpers/ApiMessages.php : add custom messages in constants REGISTER_FAILURE_* :   - UNKNOWN_USER if no user is found ;   - PASSWORD_ERROR if the password doesn't match with the given user's password

--- src/Helpers/ApiMessages.php
+++ src/Helpers/ApiMessages.php
@@ -7,6 +7,7 @@ readonly class ApiMessages
 
     //comptes utilisateurs
     const ACCOUNT_CREATION_FAILURE = "Une erreur est survenue pendant la création du compte. Veuillez réessayer ultérieurement.";
-
+    const REGISTER_FAILURE_UNKNOWN_USER = "Veuillez vérifier votre identifiant.";
+    const REGISTER_FAILURE_PASSWORD_ERROR = "Veuillez vérifier votre mot de passe.";
 
 }
